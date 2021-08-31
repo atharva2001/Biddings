c7770fa25fc36bfc0172c90af5d8253bf2057a49
unknown <atharvashirke77@gmail.com>
2021-08-31 18:19:50 +0530

    template try

--- Bidwars/urls.py
+++ Bidwars/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from django.conf.urls import url
-from Biddings import views
+from Bidwars import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('https://biddings1.herokuapp.com', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
