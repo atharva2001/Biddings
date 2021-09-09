89574164b97a30c583f5651bbd14fded2808064f
unknown <atharvashirke77@gmail.com>
2021-09-09 19:22:45 +0530

    image bugs

--- Biddings/urls.py
+++ Biddings/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     # path('search', include('Bidwars.urls')),
     path('/register', include('Bidwars.urls')),
     path('', include('Bidwars.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
