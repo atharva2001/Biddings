098a0945968c3f7b123643e7b2e998e893468997
Atharva Shirke <79209169+atharva2001@users.noreply.github.com>
2021-10-08 12:09:07 +0530

    Update settings.py

--- Biddings/settings.py
+++ Biddings/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 # STATICFILES_DIRS = [
 #     ("media", "/media/"),  
@@ -132,7 +132,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
     
 # AwS mf
