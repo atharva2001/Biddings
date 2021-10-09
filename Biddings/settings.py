66198d9526ce1e8d432dbd5f43131961a57f571f
unknown <atharvashirke77@gmail.com>
2021-10-09 10:50:02 +0530

    aws mf

--- Biddings/settings.py
+++ Biddings/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 # STATICFILES_DIRS = [
 #     ("media", "/media/"),  
@@ -137,14 +137,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
     
 # AwS mf
 
-AWS_ACCESS_KEY_ID = 'AKIAYOKHOT5XSV55WSWN'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'bidwars-bucks'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETER = {'CacheControl' : 'max-age=86400'}
-AWS_LOCATION = 'static'
-AWS_S3_FILE_OWERWITE = False
-AWS_DEFAULT_ACL = 'public-read'
-DEFAULT_FILE_STORAGE = 'Biddings.storages.MediaStore'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
